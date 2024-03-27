c97bb26f3c98d32e44214981262ff1c37b77c0c4
nero-bc <126649764+nero-bc@users.noreply.github.com>
2024-03-27 21:01:01 +0530

    Update config.py

--- config.py
+++ config.py
@@ -13,10 +13,10 @@ load_dotenv()
 
 class Config(object):
     # Get a token from @BotFather
-    BOT_TOKEN = os.environ.get("BOT_TOKEN")
+    BOT_TOKEN = os.environ.get("BOT_TOKEN", "7160412742:AAEn232qJLYj3gKFXpk-GupXaNmLyOhlg3U")
     # The Telegram API things
-    API_ID = os.environ.get("API_ID")
-    API_HASH = os.environ.get("API_HASH")
+    API_ID = os.environ.get("API_ID", "25695562")
+    API_HASH = os.environ.get("API_HASH", "0b691c3e86603a7e34aae0b5927d725a")
     # Get these values from my.telegram.org
     # Array to store users who are authorized to use the bot
 
@@ -30,12 +30,12 @@ class Config(object):
     CHUNK_SIZE = int(os.environ.get("CHUNK_SIZE", 128))
     # Proxy for accessing youtube-dl in GeoRestricted Areas
     # Get your own proxy from https://github.com/rg3/youtube-dl/issues/1091#issuecomment-230163061
-    HTTP_PROXY = os.environ.get("HTTP_PROXY", "")
+    HTTP_PROXY = os.environ.get("HTTP_PROXY", "103.44.116.90:3128")
 
     # Set timeout for subprocess
     PROCESS_MAX_TIMEOUT = 3700
 
-    OWNER_ID = os.environ.get("OWNER_ID")
+    OWNER_ID = os.environ.get("OWNER_ID", "1895952308")
     ADL_BOT_RQ = {}
-    AUTH_USERS = list({int(x) for x in os.environ.get("AUTH_USERS", "0").split()})
+    AUTH_USERS = list({int(x) for x in os.environ.get("AUTH_USERS", "1895952308").split()})
     AUTH_USERS.append(OWNER_ID)
